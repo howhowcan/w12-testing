34bfef426d8bca2de33fb4a9e7165deab32beb0d
tsengch <tsengch@cs.nctu.edu.tw>
2022-05-16 19:57:16 +0800

    first test

---
+++ apps/bowling/src/app/game.ts
@@ -0,0 +1,11 @@
+export class Game {
+    // private _score = 0;
+    roll(pins: number) {
+        // this._score += pins;
+        throw new Error('Method not implemented');
+    }
+
+    get score() {
+      return -1;
+    }
+}
